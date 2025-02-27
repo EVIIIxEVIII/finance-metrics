9731270f04cd707a415b3643a2bb894787fe3033
Kirill Katz <cat.chirill@gmail.com>
2025-02-27 14:49:59 +0200

    Add xi and kappa

--- compute_heston_params.py
+++ compute_heston_params.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 data = pd.read_csv("clean_ibm.csv")
 
@@ -8,24 +8,36 @@ data['Date'] = pd.to_datetime(data['Date'], format="ISO8601")
 data = data.sort_values(by='Date')
 data['HLOC'] = (data['Open'] + data['Close'] + data['Low'] + data['High']) / 4
 data['Log_return'] = np.log(data['HLOC'] / data['HLOC'].shift(1))
+data['Variance'] = data['Log_return']**2
 
+data.dropna(inplace=True)
 
 def compute_mu():
     mu = data['Log_return'].mean()
-    return mu
+    return mu * 252
 
 def compute_theta():
-    data['Realized_volatility_20d'] = data['Log_return'].rolling(window=20).std()
-    data['Variance_20d'] = data['Realized_volatility_20d']**2
-    theta = data['Variance_20d'].mean()
-    return theta
-
+    theta = data['Variance'].mean()
+    return theta * 252
 
 def compute_kappa():
-    pass
+    data['Variance_lagged'] = data['Variance'].shift(1)
+    data.dropna(inplace=True)
+
+    X = data['Variance_lagged']
+    Y = data['Variance']
+
+    X = sm.add_constant(X)
+    model = sm.OLS(Y, X).fit()
+    b = model.params[1]
+
+    kappa = 1 - b
+    return kappa
 
 def compute_xi():
-    pass
+    data['Variance_change'] = data['Variance'].diff()
+    xi = data['Variance_change'].std()
+    return xi * np.sqrt(252)
 
 def compute_rho():
     pass
@@ -33,7 +45,10 @@ def compute_rho():
 
 mu = compute_mu()
 theta = compute_theta()
+kappa = compute_kappa()
+xi = compute_xi()
+
 print(f"The mu:     {mu}")
 print(f"The theata: {theta}")
-print(f"The annualized theata: {theta * 252}")
-
+print(f"The kappa:  {kappa}")
+print(f"The xi:     {xi}")
