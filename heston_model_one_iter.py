69892039332aba76a26da3b984a9a98bd6a67fb1
Kirill Katz <cat.chirill@gmail.com>
2025-02-27 19:07:31 +0200

    Use the computed params in the Heston model

--- heston_model_one_iter.py
+++ heston_model_one_iter.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Heston Model Parameters
-S0 = 181.77     # Initial stock price
+S0 = 200     # Initial stock price
 v0 = 0.04    # Initial variance
 
-mu = 0.05    # Expected return
-theta = 0.04 # Long-term variance
-kappa = 2.0  # Mean reversion speed
-xi = 0.3     # Volatility of volatility
+mu = 0.010440259988853659    # Expected return
+theta = 0.0875747520494337 # Long-term variance
+kappa = 0.9105743607865966  # Mean reversion speed
+xi = 0.15456171818321993     # Volatility of volatility
 rho = -0.7   # Correlation between dW_S and dW_v
 dt = 1 / 252 # One trading day
 
@@ -19,7 +19,7 @@ v = v0
 price = [S0]
 vol = [v0]
 
-for i in range(252):
+for i in range(504):
     Z1 = np.random.normal(0, 1)   # Standard normal for stock price
     Z2 = np.random.normal(0, 1)   # Independent standard normal
     Zs = Z1
@@ -28,16 +28,16 @@ for i in range(252):
     v = max(v + kappa * (theta - v) * dt + xi * np.sqrt(v * dt) * Zv, 1e-8)
     S = S * np.exp((mu - 0.5 * v) * dt + np.sqrt(v * dt) * Zs)
 
-    # price.append(S)
+    price.append(S)
     vol.append(v)
 
     print(f"Next volatility: {np.sqrt(v):.6f}")
 
 
-# plt.figure()
-# plt.xlabel("Time")
-# plt.ylabel("Price")
-# plt.plot(price)
+plt.figure()
+plt.xlabel("Time")
+plt.ylabel("Price")
+plt.plot(price)
 
 plt.figure()
 plt.xlabel("Time")
